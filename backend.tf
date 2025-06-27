fe96a71fb48c24fc2e7a36c09fcee2da7d7cfeda
Gholam Alam <luqustra@gmail.com>
2025-06-28 09:38:16 +1000

    Add Azure remote backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,8 @@
-# Backend
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "azurerm" {
+    resource_group_name   = "PTutorial-rg"           # Your actual resource group name
+    storage_account_name  = "ptutorial123"           # Your Azure Storage Account name
+    container_name        = "ptstate"                # Blob container name for state files
+    key                   = "terraform.tfstate"      # State file name
   }
 }
